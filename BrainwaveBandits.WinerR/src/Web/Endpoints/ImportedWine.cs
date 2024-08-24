425b813a3ccacc40964af54c9bed0dce328f2979
Patric Martin <patric.martin@garaio.com>
2024-08-24 15:54:16 +0200

    hangfire import

--- BrainwaveBandits.WinerR/src/Web/Endpoints/ImportedWine.cs
+++ BrainwaveBandits.WinerR/src/Web/Endpoints/ImportedWine.cs
@@ -1,8 +1,5 @@
-﻿using System.Globalization;
-using BrainwaveBandits.WinerR.Application.ImportedWines.Commands.CreateImportedWine;
-using BrainwaveBandits.WinerR.Application.ImportedWines.Commands.ImportWines;
+﻿using BrainwaveBandits.WinerR.Application.ImportedWines.Commands.ImportWines;
 using BrainwaveBandits.WinerR.Web.Tasks.ImportWine;
-using CsvHelper;
 
 namespace BrainwaveBandits.WinerR.Web.Endpoints;
 
@@ -14,30 +11,8 @@ public class ImportedWine : EndpointGroupBase
             .MapPost(Import);
     }
 
-    public async Task<int> Import(ISender sender, ImportWinesCommand command)
+    public Task<int> Import(ISender sender, ImportWinesCommand command)
     {
-        string filePath = Path.Combine(Directory.GetCurrentDirectory(), "App_Data", "wines.csv");
-        using (StreamReader reader = new StreamReader(filePath))
-        using (CsvReader csv = new CsvReader(reader, CultureInfo.InvariantCulture))
-        {
-            csv.Context.RegisterClassMap<ImportedWineMap>();
-            IAsyncEnumerable<ImportedWineDto> importedWineRecords = csv.GetRecordsAsync<ImportedWineDto>();
-            List<ImportedWineDto> importedWineDtos = await importedWineRecords.ToListAsync();
-            CreateImportedWineCommand createImportedWineCommand;
-            foreach (ImportedWineDto importedWineDto in importedWineDtos)
-            {
-                createImportedWineCommand = new()
-                {
-                    WineId = importedWineDto.WineId,
-                    WineName = importedWineDto.WineName,
-                    WineryName = importedWineDto.WineryName,
-                    Vintages = importedWineDto.Vintages
-                };
-
-                await sender.Send(createImportedWineCommand);
-            }
-
-            return importedWineDtos.Count;
-        }
+        return Task.FromResult(0);
     }
 }
