ad9bbbffd46c2945f36a071d3d666d23fb0f9b9c
Dres Baumann <dres.baumann@garaio.com>
2024-08-24 19:44:58 +0200

    Change Nswag config

--- BrainwaveBandits.WinerR/src/Application/Wines/Commands/CreateWineByVoice/UploadAudioFileCommandHandler.cs
+++ BrainwaveBandits.WinerR/src/Application/Wines/Commands/CreateWineByVoice/UploadAudioFileCommandHandler.cs
@@ -1,9 +1,4 @@
-﻿using System.Net.Http;
-using System.Net.Http.Json;
-using System.Text.Json;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Collections.Generic;
+﻿using System.Net.Http.Json;
 
 public class UploadAudioFileCommandHandler : IRequestHandler<UploadAudioFileCommand, List<string>>
 {
